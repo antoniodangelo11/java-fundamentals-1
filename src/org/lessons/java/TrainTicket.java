1abb5c19cfcf78a81941a39f84973189e10d873b
Antonio D'Angelo <reapercsp@gmail.com>
2023-10-18 16:22:52 +0200

    changed variables in TicketTrain

--- src/org/lessons/java/TrainTicket.java
+++ src/org/lessons/java/TrainTicket.java
@@ -15,14 +15,22 @@ public class TrainTicket {
         System.out.print("Inserisci l'età del passeggero: ");
         int Age = input.nextInt();
 
+        // Dichiaro la variabile del carburante
+        double fuelPrice = 0.21;
+
         // Calcolo il prezzo base senza sconti
-        double basePrice = Km * 0.21;
+        double basePrice = Km * fuelPrice;
+
+        // Dichiaro le variabili per gli sconti
+        double juniorDiscount = 0.20;
+        double seniorDiscount = 0.40;
+
 
         // Applico gli sconti in base all'età
         if (Age < 18) {
-            basePrice -= basePrice * 0.20;
+            basePrice -= basePrice * juniorDiscount;
         } else if (Age >= 65) {
-            basePrice -= basePrice * 0.40;
+            basePrice -= basePrice * seniorDiscount;
         }
 
         System.out.println("Il prezzo totale del viaggio è: " + basePrice + " €");
