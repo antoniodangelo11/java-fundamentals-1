70b9deae78a529b38ee17436e5cd4f9045886094
Antonio D'Angelo <reapercsp@gmail.com>
2023-10-19 09:16:22 +0200

    add comment

--- src/org/lessons/java/PartyHouse.java
+++ src/org/lessons/java/PartyHouse.java
@@ -5,15 +5,26 @@ public class PartyHouse {
     public static void main(String[] args) {
 
         // Inizializzo l'array con i nomi
-        String[] guestList = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi",
-                "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+        String[] guestList = {
+                "Dua Lipa",
+                "Paris Hilton",
+                "Manuel Agnelli",
+                "J-Ax",
+                "Francesco Totti",
+                "Ilary Blasi",
+                "Bebe Vio",
+                "Luis",
+                "Pardis Zarei",
+                "Martina Maccherone",
+                "Rachel Zeilic"
+        };
 
         // Richiedo il nome all'utente
         Scanner input = new Scanner(System.in);
         System.out.print("Scrivi il tuo nome: ");
         String userName = input.nextLine();
 
-        // Verifico se il nome è presente nella lista o meno
+        // Verifico se il nome è presente nella lista o meno con metodo for
         /*boolean invited = false;
         for (int i = 0; i < guestList.length; i++) {
             if (userName.equals(guestList[i])) {
@@ -22,6 +33,7 @@ public class PartyHouse {
             }
         }*/
 
+        // Verifico se il nome è presente nella lista o meno con metodo while
         boolean invited = false;
         int i = 0;
         while (i < guestList.length) {
